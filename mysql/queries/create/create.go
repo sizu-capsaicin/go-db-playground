1cb78613c3244c051570c321f7cb7ba5063f8b27
sizu <sizu@inl.ics.keio.ac.jp>
2020-07-25 22:33:37 +0900

    fix create

--- mysql/queries/create/create.go
+++ mysql/queries/create/create.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"flag"
-	"fmt"
 	"log"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -32,35 +31,46 @@ func main() {
 
 	// データベースが既に作成済みであれば一度 drop する
 	query := "drop database if exists " + *dbName
-	r, err := db.Exec(query)
+	_, err = db.Exec(query)
 	if err != nil {
 		log.Fatalf("Faild to exec query: %s\n", query)
 	}
-	fmt.Println(r)
 
 	// データベースの作成
 	query = "create database " + *dbName
-	r, err = db.Exec(query)
+	_, err = db.Exec(query)
 	if err != nil {
 		log.Fatalf("Faild to exec query: %s\n", query)
 	}
-	fmt.Println(r)
 
 	// データベースのスイッチ
 	query = "use " + *dbName
-	r, err = db.Exec(query)
+	_, err = db.Exec(query)
 	if err != nil {
 		log.Fatalf("Faild to exec query: %s\n", query)
 	}
-	fmt.Println(r)
 
 	// テーブルが既に作成済みであれば一度 drop する
-	query = "drop table if exists companies, safeties, lines, trains"
-	r, err = db.Exec(query)
+	query = "drop table if exists companies"
+	_, err = db.Exec(query)
 	if err != nil {
 		log.Fatalf("Faild to exec query: %s\n", query)
 	}
-	fmt.Println(r)
+	query = "drop table if exists safeties"
+	_, err = db.Exec(query)
+	if err != nil {
+		log.Fatalf("Faild to exec query: %s\n", query)
+	}
+	query = "drop table if exists lines"
+	_, err = db.Exec(query)
+	if err != nil {
+		log.Fatalf("Faild to exec query: %s\n", query)
+	}
+	query = "drop table if exists trains"
+	_, err = db.Exec(query)
+	if err != nil {
+		log.Fatalf("Faild to exec query: %s\n", query)
+	}
 
 	// テーブルの作成
 	query = "create table companies("
@@ -69,21 +79,19 @@ func main() {
 	query += "location varchar(50), "
 	query += "url varchar(50)"
 	query += ") charset=utf8"
-	r, err = db.Exec(query)
+	_, err = db.Exec(query)
 	if err != nil {
 		log.Fatalf("Faild to exec query: %s\n", query)
 	}
-	fmt.Println(r)
 
 	query = "create table safeties("
 	query += "id int auto_incremet not null primary key, "
 	query += "name varchar(50) "
 	query += ") charset=utf8"
-	r, err = db.Exec(query)
+	_, err = db.Exec(query)
 	if err != nil {
 		log.Fatalf("Faild to exec query: %s\n", query)
 	}
-	fmt.Println(r)
 
 	query = "create table lines("
 	query += "id int auto_incremet not null primary key, "
@@ -92,16 +100,20 @@ func main() {
 	query += "rail_range int, "
 	query += "stations int, "
 	query += "safety_id int not null"
+	query += "operator_id int not null"
 	query += "constraint fk_safety_id "
 	query += "foreign key (safety_id) "
 	query += "references safeties (id) "
+	query += "on delete restrict on update restrict, "
+	query += "constraint fk_operator_id "
+	query += "foreign key (operator_id) "
+	query += "references companies (id) "
 	query += "on delete restrict on update restrict"
 	query += ") charset=utf8"
-	r, err = db.Exec(query)
+	_, err = db.Exec(query)
 	if err != nil {
 		log.Fatalf("Faild to exec query: %s\n", query)
 	}
-	fmt.Println(r)
 
 	query = "create table trains("
 	query += "id int auto_incremet not null primary key, "
@@ -124,9 +136,8 @@ func main() {
 	query += "references safeties (id) "
 	query += "on delete restrict on update restrict"
 	query += ") charset=utf8"
-	r, err = db.Exec(query)
+	_, err = db.Exec(query)
 	if err != nil {
 		log.Fatalf("Faild to exec query: %s\n", query)
 	}
-	fmt.Println(r)
 }
