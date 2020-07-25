3dbfd21fa543e4eb414f186176147b1eb5bd8105
sizu-capsaicin <sizu@inl.ics.keio.ac.jp>
2020-07-25 09:49:48 +0000

    merging

--- mysql/queries/create/create.go
+++ mysql/queries/create/create.go
@@ -4,12 +4,14 @@ import (
 	"database/sql"
 	"flag"
 	"log"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 const (
-	mysqlPath = "tcp(127.0.0.1:3333)/"
+	mysqlPath = "tcp(127.0.0.1:3306)/"
 	userName  = "root"
-	passwd    = "xxxx"
+	passwd    = "root"
 )
 
 func main() {
@@ -26,16 +28,18 @@ func main() {
 	defer db.Close()
 
 	// データベースの作成
-	query := "create database " + *dbName
+	query := "CREATE DATABASE " + *dbName + ";"
 	_, err = db.Exec(query)
 	if err != nil {
 		log.Fatalf("Faild to exec query: %s\n", query)
 	}
 
 	// テーブルの作成
+/*
 	query = "create table train()" + *dbName
 	_, err = db.Exec(query)
 	if err != nil {
 		log.Fatalf("Faild to exec query: %s\n", query)
 	}
+*/
 }
