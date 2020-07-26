d9670e9bcf301a1e30ca773e2ea7340a11b069ad
sizu <sizu@inl.ics.keio.ac.jp>
2020-07-27 04:04:12 +0900

    make mongo/acid

---
+++ mongo/queries/acid/acid.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log"
+
+	"github.com/globalsign/mgo/bson"
+	"github.com/sizu-capsaicin/go-db-playground/mongo"
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	// 並行処理
+	eg := errgroup.Group{}
+	eg.Go(func() error {
+		findAndUpdate(-10)
+		return nil
+	})
+	eg.Go(func() error {
+		findAndUpdate(+10)
+		return nil
+	})
+	if err := eg.Wait(); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func findAndUpdate(update int) {
+	// サンプルデータの取得
+	var r []mongo.Data
+	q := bson.M{"train.name": "京急 2100 形"}
+
+	// find
+	mongo.FindAll(&r, q)
+
+	// max-speed の抜き出し
+	maxSpeed := r[0].Train.MaxSpeed
+	// セレクタ・更新内容の設定
+	s := bson.M{"train.name": "京急 2100 形"}
+	u := bson.M{"$set": bson.M{"train.max-speed": maxSpeed + update}}
+
+	// 更新
+	mongo.Update(s, u)
+}
