4f071d191ddb6edb2a115d935502352c4bd76518
sizu <sizu@inl.ics.keio.ac.jp>
2020-07-27 01:19:08 +0900

    update mongo/find

--- mongo/queries/find/find-all/find-all.go
+++ mongo/queries/find/find-all/find-all.go
@@ -27,21 +27,25 @@ func main() {
 			str += "  |- raild-width: " + strconv.Itoa(l.RailWidth) + " mm\n"
 			str += "  |- raild-range: " + strconv.FormatFloat(l.RailRange, 'f', 1, 64) + " km\n"
 			str += "  |- stations: " + strconv.Itoa(l.Stations) + "\n"
-			str += "  |- safeties\n"
+			str += "  |- line-safeties\n"
 			for _, s := range l.Safeties {
 				str += "    |- name: " + s.Name + "\n"
 			}
-			str += "  |- operator\n"
+			str += "  |- line-operator\n"
 			str += "    |- name: " + l.Operator.Name + "\n"
 			str += "    |- location: " + l.Operator.Location + "\n"
 			str += "    |- URL: " + l.Operator.URL + "\n"
 		}
-		str += "|- operator\n"
+		str += "|- train-operator\n"
 		str += "  |- name: " + t.Operator.Name + "\n"
 		str += "  |- location: " + t.Operator.Location + "\n"
 		str += "  |- URL: " + t.Operator.URL + "\n"
 		str += "|- max-speed: " + strconv.Itoa(t.MaxSpeed) + " km/h\n"
 		str += "|- acceleration: " + strconv.FormatFloat(t.Acceleration, 'f', 1, 64) + " km/h/s\n"
+		str += "|- train-safeties\n"
+		for _, s := range r.Train.Safeties {
+			str += "  |- name: " + s.Name + "\n"
+		}
 	}
 	fmt.Print(str)
 }
