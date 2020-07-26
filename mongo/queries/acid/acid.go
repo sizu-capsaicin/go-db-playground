1a2885535e14ccb105e282c2a0e0e41a88bb3204
sizu <sizu@inl.ics.keio.ac.jp>
2020-07-27 06:13:39 +0900

    update acid

--- mongo/queries/acid/acid.go
+++ mongo/queries/acid/acid.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"time"
 
+	"github.com/globalsign/mgo"
 	"github.com/globalsign/mgo/bson"
 	"github.com/sizu-capsaicin/go-db-playground/mongo"
 	"golang.org/x/sync/errgroup"
+)
+
+const (
+	mongoPath = "mongodb://localhost/"
+	dbName    = "adb"
+	path      = mongoPath + dbName
+	cName     = "train"
 )
 
 func main() {
@@ -26,25 +35,33 @@ func main() {
 }
 
 func findAndUpdate(update int) {
+	// MongoDB との接続
+	s, _ := mgo.Dial(path)
+	defer s.Close()
+	db := s.DB(dbName)
+
 	// サンプルデータの取得
 	var r []mongo.Data
 	q := bson.M{"train.name": "京急 2100 形"}
 
 	// find
-	mongo.FindAll(&r, q)
-
-	// 一旦寝かせる
-	time.Sleep(time.Second * 10)
+	db.C(cName).Find(q).All(&r)
 
 	// max-speed の抜き出し
 	maxSpeed := r[0].Train.MaxSpeed
 	// セレクタ・更新内容の設定
-	s := bson.M{"train.name": "京急 2100 形"}
+	selector := bson.M{"train.name": "京急 2100 形"}
 	u := bson.M{"$set": bson.M{"train.maxspeed": maxSpeed + update}}
 
 	// 一旦寝かせる
 	time.Sleep(time.Second * 10)
 
 	// 更新
-	mongo.Update(s, u)
+	info, err := db.C(cName).UpdateAll(selector, u)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if info != nil {
+		fmt.Printf("%+v\n", info)
+	}
 }
